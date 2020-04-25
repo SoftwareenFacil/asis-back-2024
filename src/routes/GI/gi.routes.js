66a35f39e0ca1689d24d80313b0b11bffb6f67f2
mcasanovar <mariocasanovaramirez@gmail.com>
2020-04-24 23:03:22 -0400

    add route gi to insert, select and delete

--- src/routes/GI/gi.routes.js
+++ src/routes/GI/gi.routes.js
@@ -12,4 +12,20 @@ router.get('/', async (req, res) =>{
     res.json(result)
 });
 
+//INSERT
+router.post('/', async (req, res) => {
+    const db = await connect()
+    const newGi = req.body
+    const result = await db.collection('gi').insertOne(newGi);
+    res.json(result)
+});
+
+//DELETE
+router.delete('/:id', async (req, res) =>{
+    const {id} = req.params;
+    const db = await connect()
+    const result = await db.collection('gi').deleteOne({_id: ObjectID(id)})
+    res.json(result)
+})
+
 export default router;
---
+++ src/routes/solicitudes/solicitudes.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+const router = Router();
+
+//database connection
+import { connect } from "../../database";
+import { ObjectID } from "mongodb";
+
+//SELECT
+router.get('/', async (req, res) => {
+    const db = await connect();
+    const result = await db.collection('solicitudes').find({}).toArray();
+    res.json(result);
+})
+
+export default router;
