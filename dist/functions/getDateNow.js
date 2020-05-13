3c78dd406bface70db5113a0a33c8985b350353b
mcasanovar <mariocasanovaramirez@gmail.com>
2020-05-12 21:38:17 -0400

    olvide build, subo denuevo

--- dist/functions/getDateNow.js
+++ dist/functions/getDateNow.js
@@ -10,9 +10,8 @@ function getDate() {
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
       year = d.getFullYear(),
-      hour = d.getHours(),
-      minutes = d.getMinutes(),
-      seconds = d.getSeconds();
+      hour = d.getHours().toLocaleString(),
+      minutes = d.getMinutes().toLocaleString();
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day; // let fecha = `${f.toISOString().slice(0, 10)} ${f.toISOString().slice(11, f.toString().length)}`
   // let fecha = f.toLocaleDateString('cl-CL').toISO
