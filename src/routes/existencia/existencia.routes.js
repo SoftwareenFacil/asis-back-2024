2a531f892c5906d9977307a7dc5a5b815635112f
mario casanova ramirez <mariocasanovaramirez@gmail.com>
2020-06-11 22:12:17 -0400

    added route consultar existencia to insert salidas

--- src/routes/existencia/existencia.routes.js
+++ src/routes/existencia/existencia.routes.js
@@ -16,4 +16,33 @@ router.get('/', async (req, res) =>{
     res.json(result)
 });
 
+//CONSULTAR POR ENTRADAS PARA INSERCION DE SALIDAS
+router.post('/consultar', async (req, res) =>{
+    const db = await connect()
+    const {code, salidas} = req.body
+    const result = await db.collection('existencia').findOne({codigo_categoria_tres: code})
+    if(result == null){
+        res.json({
+            message: "La Subcategoria 3 consultada no existe en la existencia del sistema",
+            isOK: false
+        })
+    }
+    else{
+        if((result.entradas - result.salidas) < salidas){
+            res.json({
+                message: "La cantidad de salidas a insertar supera las entradas en el sistema",
+                isOk: false,
+                cupos_disponibles: result.entradas - result.salidas,
+                salida_ingresada: salidas
+            })
+        }
+        else{
+            res.json({
+                message: "Cantidad aceptada para ingreso",
+                isOk: true
+            })
+        }
+    }
+})
+
 export default router;
