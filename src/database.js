c9d4035b2ed0253516ad60b2d38b87baa2eab1b7
mario <mariocasanovaramirez@gmail.com>
2021-05-05 23:31:19 -0400

    testing with db remote

--- src/database.js
+++ src/database.js
@@ -4,14 +4,14 @@ import { MONGODB_CONNECTION_DEV, MONGODB_CLIENT_DEV } from "./constant/var";
 export async function connect(){
     try {
         // const local = 'mongodb://localhost/local-db-asis';
-        const connextionMongodb = process.env.MONGODB_CONNECTION_PROD || MONGODB_CONNECTION_DEV;
-        // const atlasMongo = 'mongodb+srv://admin:asis2020@cluster0-ftqrl.mongodb.net/test?retryWrites=true&w=majority';
-        const client = await MongoClient.connect(connextionMongodb, {
+        // const connextionMongodb = process.env.MONGODB_CONNECTION_PROD || MONGODB_CONNECTION_DEV;
+        const atlasMongo = 'mongodb+srv://admin:Karla2021@cluster0.3pzmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+        const client = await MongoClient.connect(atlasMongo, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
 
-        const db = client.db(process.env.MONGODB_CLIENT_PROD || MONGODB_CLIENT_DEV);
+        const db = client.db('asis-db');
         console.log('DB connected')
         return db;
 
