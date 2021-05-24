6ebf320707867bba1243e94655bed1ff17222500
mario <mariocasanovaramirez@gmail.com>
2021-05-24 02:28:59 -0400

    go to production

--- src/database.js
+++ src/database.js
@@ -3,9 +3,9 @@ import { MONGODB_CONNECTION_DEV, MONGODB_CLIENT_DEV } from "./constant/var";
 
 export async function connect(){
     try {
-        const mongoUri = 'mongodb://localhost/local-db-asis';
+        // const mongoUri = 'mongodb://localhost/local-db-asis';
         // const connextionMongodb = process.env.MONGODB_CONNECTION_PROD || MONGODB_CONNECTION_DEV;
-        // const mongoUri = 'mongodb+srv://admin:Karla2021@cluster0.3pzmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+        const mongoUri = 'mongodb+srv://admin:Karla2021@cluster0.3pzmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
         const client = await MongoClient.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
