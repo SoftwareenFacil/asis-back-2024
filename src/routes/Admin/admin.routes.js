4052da04c770320da7d62fc1bcf76120ac286d47
mario <mariocasanovaramirez@gmail.com>
2022-03-07 22:13:22 -0300

    request admin is done

--- src/routes/Admin/admin.routes.js
+++ src/routes/Admin/admin.routes.js
@@ -73,7 +73,8 @@ router.put('/solicitudes/:id', async (req, res) => {
     hora_servicio_solicitado,
     fecha_servicio_solicitado_termino,
     hora_servicio_solicitado_termino,
-    jornada
+    jornada,
+    id_GI_PersonalAsignado
   } = req.body
   const conn = await connect();
   const db = conn.db('asis-db');
@@ -82,107 +83,140 @@ router.put('/solicitudes/:id', async (req, res) => {
 
     console.log([monto_neto, porcentaje_impuesto, valor_impuesto, exento, monto_total])
 
-    await db.collection('solicitudes').updateOne({ _id: ObjectID(id) }, {
-      $set: {
-        fecha_solicitud,
-        mes_solicitud,
-        anio_solicitud,
-        categoria1,
-        categoria2,
-        categoria3,
-        nombre_servicio,
-        descripcion_servicio,
-        monto_neto,
-        porcentaje_impuesto,
-        valor_impuesto,
-        exento,
-        monto_total,
-        nro_contrato_seleccionado_cp,
-        faena_seleccionada_cp,
-        fecha_servicio_solicitado,
-        hora_servicio_solicitado,
-        fecha_servicio_solicitado_termino,
-        hora_servicio_solicitado_termino,
-        jornada
-      }
-    });
+    const requestFinded = await db.collection("solicitudes").findOne({ _id: ObjectID(id), isActive: true })
 
-    await db.collection('reservas').updateOne({ codigo: codigo.replace("SOL", "AGE") }, {
-      $set: {
-        nombre_servicio,
-        valor_servicio: monto_total,
-        faena_seleccionada_cp,
-        fecha_reserva: fecha_servicio_solicitado,
-        hora_reserva: hora_servicio_solicitado,
-        fecha_reserva_fin: fecha_servicio_solicitado_termino,
-        hora_reserva_fin: hora_servicio_solicitado_termino
-      }
-    });
+    if(requestFinded){
 
-    await db.collection('evaluaciones').updateOne({ codigo: codigo.replace("SOL", "EVA") }, {
-      $set: {
-        nombre_servicio,
-        valor_servicio: monto_total,
-        faena_seleccionada_cp,
-        fecha_evaluacion: fecha_servicio_solicitado,
-        fecha_evaluacion_fin: fecha_servicio_solicitado_termino,
-        hora_inicio_evaluacion: hora_servicio_solicitado,
-        hora_termino_evaluacion: hora_servicio_solicitado_termino
-      }
-    });
+      await db.collection('solicitudes').updateOne({ _id: ObjectID(id), isActive: true }, {
+        $set: {
+          id_GI_PersonalAsignado,
+          fecha_solicitud,
+          mes_solicitud,
+          anio_solicitud,
+          categoria1,
+          categoria2,
+          categoria3,
+          nombre_servicio,
+          descripcion_servicio,
+          monto_neto,
+          porcentaje_impuesto,
+          valor_impuesto,
+          exento,
+          monto_total,
+          nro_contrato_seleccionado_cp,
+          faena_seleccionada_cp,
+          fecha_servicio_solicitado,
+          hora_servicio_solicitado,
+          fecha_servicio_solicitado_termino,
+          hora_servicio_solicitado_termino,
+          jornada
+        }
+      });
 
-    await db.collection('resultados').updateOne({ codigo: codigo.replace("SOL", "RES") }, {
-      $set: {
-        nombre_servicio,
-        valor_servicio: monto_total,
-        faena_seleccionada_cp
+      const reservationFinded = db.collection('reservas').findOne({ codigo: codigo.replace("SOL", "AGE"), isActive: true })
+  
+      if(reservationFinded){
+        await db.collection('reservas').updateOne({ codigo: codigo.replace("SOL", "AGE"), isActive: true }, {
+          $set: {
+            id_GI_personalAsignado: id_GI_PersonalAsignado,
+            nombre_servicio,
+            valor_servicio: monto_total,
+            faena_seleccionada_cp,
+            fecha_reserva: fecha_servicio_solicitado,
+            hora_reserva: hora_servicio_solicitado,
+            fecha_reserva_fin: fecha_servicio_solicitado_termino,
+            hora_reserva_fin: hora_servicio_solicitado_termino
+          }
+        });
       }
-    });
 
-    const invoice = await db.collection('facturaciones').findOne({ codigo: codigo.replace("SOL", "FAC") })
+      const evaluationFinded = await db.collection('evaluaciones').findOne({ codigo: codigo.replace("SOL", "EVA"), isActive: true })
 
-    await db.collection('facturaciones').updateOne({ codigo: codigo.replace("SOL", "FAC") }, {
-      $set: {
-        nombre_servicio,
-        faena_seleccionada_cp,
-        valor_servicio: monto_total,
-        monto_neto,
-        porcentaje_impuesto,
-        valor_impuesto,
-        exento,
-        sub_total: monto_total,
-        total: monto_total - (invoice.descuento ?? 0)
+      if(evaluationFinded){
+        await db.collection('evaluaciones').updateOne({ codigo: codigo.replace("SOL", "EVA"), isActive: true }, {
+          $set: {
+            id_GI_personalAsignado: id_GI_PersonalAsignado,
+            nombre_servicio,
+            valor_servicio: monto_total,
+            faena_seleccionada_cp,
+            fecha_evaluacion: fecha_servicio_solicitado,
+            fecha_evaluacion_fin: fecha_servicio_solicitado_termino,
+            hora_inicio_evaluacion: hora_servicio_solicitado,
+            hora_termino_evaluacion: hora_servicio_solicitado_termino
+          }
+        });
       }
-    });
-
-    await db.collection('pagos').updateOne({ codigo: codigo.replace("SOL", "PAG") }, {
-      $set: {
-        nombre_servicio,
-        valor_servicio: monto_total,
-        faena_seleccionada_cp
+  
+      const resultFinded = await db.collection('resultados').findOne({ codigo: codigo.replace("SOL", "RES"), isActive: true })
+  
+      if(resultFinded){
+        await db.collection('resultados').updateOne({ codigo: codigo.replace("SOL", "RES"), isActive: true }, {
+          $set: {
+            id_GI_personalAsignado: id_GI_PersonalAsignado,
+            nombre_servicio,
+            valor_servicio: monto_total,
+            faena_seleccionada_cp
+          }
+        });
       }
-    });
 
-    await db.collection('pagos').updateOne({ codigo: codigo.replace("SOL", "PAG") }, {
-      $set: {
-        nombre_servicio,
-        valor_servicio: monto_total,
-        faena_seleccionada_cp
+  
+      const invoice = await db.collection('facturaciones').findOne({ codigo: codigo.replace("SOL", "FAC"), isActive: true })
+  
+      if(invoice){
+        await db.collection('facturaciones').updateOne({ codigo: codigo.replace("SOL", "FAC"), isActive: true }, {
+          $set: {
+            id_GI_personalAsignado: id_GI_PersonalAsignado,
+            nombre_servicio,
+            faena_seleccionada_cp,
+            valor_servicio: monto_total,
+            monto_neto,
+            porcentaje_impuesto,
+            valor_impuesto,
+            exento,
+            sub_total: monto_total,
+            total: monto_total - (invoice.descuento ?? 0)
+          }
+        });
       }
-    });
 
-    await db.collection('cobranza').updateOne({ codigo: codigo.replace("SOL", "COB") }, {
-      $set: {
-        nombre_servicio,
-        faena_seleccionada_cp,
-        valor_servicio: monto_total,
-        valor_deuda: monto_total,
+      const payment = await db.collection("pagos").findOne({ codigo: codigo.replace("SOL", "PAG"), isActive: true })
+  
+      if(payment){
+        await db.collection('pagos').updateOne({ codigo: codigo.replace("SOL", "PAG"), isActive: true }, {
+          $set: {
+            id_GI_personalAsignado: id_GI_PersonalAsignado,
+            nombre_servicio,
+            valor_servicio: monto_total,
+            faena_seleccionada_cp
+          }
+        });
       }
-    });
+
+      const requestPayment = await db.collection('cobranza').findOne({ codigo: codigo.replace("SOL", "COB"), isActive: true })
+  
+      if(requestPayment){
+        await db.collection('cobranza').updateOne({ codigo: codigo.replace("SOL", "COB"), isActive: true }, {
+          $set: {
+            nombre_servicio,
+            faena_seleccionada_cp,
+            valor_servicio: monto_total,
+            valor_deuda: monto_total,
+          }
+        });
+      }
+
+
+      return res.status(200).json({
+        err: null,
+        msg: 'Solicitud editada correctamente',
+        res: []
+      });
+    }
 
     return res.status(200).json({
-      err: null,
-      msg: 'Solicitud editada correctamente',
+      err: ERROR,
+      msg: 'Solicitud no encontrada',
       res: []
     });
 
