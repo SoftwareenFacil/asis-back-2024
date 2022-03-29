8d071d53ec81e98fd868bd45105ede02ca9a1f61
mario <mariocasanovaramirez@gmail.com>
2022-03-28 23:39:09 -0300

    module admin permissions is done

--- src/database.js
+++ src/database.js
@@ -8,7 +8,7 @@ export async function connect(){
         const MONGO_PROD = MONGODB_CONNECTION_PROD;
         const MONGO_PROD_NEW = MONGODB_CONNECTION_PROD_NEW;
 
-        const client = await MongoClient.connect(MONGO_DEV, {
+        const client = await MongoClient.connect(MONGO_PROD, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
